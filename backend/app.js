d1be344d75b5b5203163b46a4f974503ad17848a
Laura Mathieu <lauramathieu.git@gmail.com>
2021-07-09 23:16:42 +0200

    Mise en place du middleware

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const saucesRoutes = require('./routes/sauces');
+
+mongoose.connect('mongodb+srv://pekocko:pecocko.password48@cluster0.jpeop.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/sauces', saucesRoutes);
 
 module.exports = app;

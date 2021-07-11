2cc54588ee7ed5f3088403cfd4b1efcd44721260
Laura Mathieu <lauramathieu.git@gmail.com>
2021-07-11 21:15:09 +0200

    connection fonctionnelle + ajout, modification et suppression des sauces fonctionnelles

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+
+const app = express();
 
 mongoose.connect('mongodb+srv://pekocko:pecocko.password48@cluster0.jpeop.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-const app = express();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,8 +21,13 @@ app.use((req, res, next) => {
     next();
   });
 
+
+
 app.use(bodyParser.json());
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;

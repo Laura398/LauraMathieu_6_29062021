e25bfb2ebebfc412edd32540a4be3b9cead8dfbe
Laura398 <80258767+Laura398@users.noreply.github.com>
2021-07-20 12:58:21 +0200

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,10 +1,14 @@
 const jwt = require('jsonwebtoken');
+const ENV = require('dotenv');
+ENV.config();
+
+console.log(process.env.JWT_KEY)
 
 /*Checking TOKEN*/
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
